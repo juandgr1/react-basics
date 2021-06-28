59ae3c402277800791494f150af6dab3d6c3559a
juandgr1 <juandgr1@hotmail.com>
2021-06-28 17:58:46 -0600

    Modulo 12 - Comunicacion entre componentess

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Propiedades from "./components/propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
+import ComunicacionComponentes from "./components/ComunicacionComponentes";
 import {EventosES6, EventosES7, MasSobreEventos} from "./components/Eventos";
 import "./App.css";
 
@@ -51,6 +52,8 @@ function App() {
           <EventosES7/>
           <hr />
           <MasSobreEventos/>
+          <hr />
+          <ComunicacionComponentes/>
         </section>
       </header>
     </div>
